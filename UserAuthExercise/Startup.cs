936440e1ef2747b556f3bf4bb1c0bdcc339d9cfc
brobo1 <armanahz95@gmail.com>
2024-01-08 22:48:48 +0100

    Refactor Startup.cs and add role seeding
    The Startup.cs file has been refactored for better organization and readability. A method to seed an 'Admin' role has been added, which will be executed at application startup. Additionally, the Identity service configuration was updated to include Identity Roles.

--- UserAuthExercise/Startup.cs
+++ UserAuthExercise/Startup.cs
@@ -6,19 +6,57 @@ using UserAuthExercise.Data;
 namespace UserAuthExercise;
 
 public class Startup {
-	public IConfiguration Configuration { get; set; }
-
 	public Startup(IConfiguration configuration) {
 		Configuration = configuration;
 	}
+
+	public IConfiguration Configuration { get; }
 
 	public void ConfigureServices(IServiceCollection services) {
 		services.AddDbContext<ApplicationDbContext>(options =>
 														options.UseSqlite(
 															Configuration.GetConnectionString("DefaultConnection")));
-		services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-				.AddRoles<IdentityRole>() 
+		services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
 				.AddEntityFrameworkStores<ApplicationDbContext>();
+		// Other service configurations...
+	}
 
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider) {
+		if (env.IsDevelopment()) {
+			app.UseDeveloperExceptionPage();
+		} else {
+			app.UseExceptionHandler("/Home/Error");
+			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+			app.UseHsts();
+		}
+		app.UseHttpsRedirection();
+		app.UseStaticFiles();
+
+		app.UseRouting();
+
+		app.UseAuthentication();
+		app.UseAuthorization();
+
+		app.UseEndpoints(endpoints => {
+			endpoints.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+		});
+
+		// Call the seed method
+		SeedRoles(serviceProvider).Wait();
+	}
+
+	private async Task SeedRoles(IServiceProvider serviceProvider) {
+		var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+		var roleExists = await roleManager.RoleExistsAsync("Admin");
+		if (!roleExists) {
+			await roleManager.CreateAsync(new IdentityRole("Admin"));
+		}
+
+		// Here, you could add code to add specific users to the new role, 
+		// using `UserManager<IdentityUser>`
+		// Generally you do not do this in the application startup; but as a demo or testing it's fine
 	}
 }
