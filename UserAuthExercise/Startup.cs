304ec3b3bf451621a03da0d13277f30214d083ef
brobo1 <armanahz95@gmail.com>
2024-01-08 21:50:22 +0100

    Add IConfiguration in Startup and update ConfigureServices
    The Startup class now includes an IConfiguration instance which is used in the ConfigureServices method. This gets the 'DefaultConnection' string from the configuration for the DbContext. This change will allow the application to retrieve the database connection string from the app configuration settings.

--- UserAuthExercise/Startup.cs
+++ UserAuthExercise/Startup.cs
@@ -6,16 +6,22 @@ using UserAuthExercise.Data;
 namespace UserAuthExercise;
 
 public class Startup {
-	public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-    
-        services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            .AddRoles<IdentityRole>()  // This allows you to use roles.
-            .AddEntityFrameworkStores<ApplicationDbContext>();
-        
-        services.AddControllersWithViews();
-        services.AddRazorPages();
-    }
+	public IConfiguration Configuration { get; set; }
+
+	public Startup(IConfiguration configuration) {
+		Configuration = configuration;
+	}
+
+	public void ConfigureServices(IServiceCollection services) {
+		services.AddDbContext<ApplicationDbContext>(options =>
+														options.UseSqlite(
+															Configuration.GetConnectionString("DefaultConnection")));
+
+		services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+				.AddRoles<IdentityRole>() // This allows you to use roles.
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+
+		services.AddControllersWithViews();
+		services.AddRazorPages();
+	}
 }
